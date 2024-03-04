3b9c3a82f2f62d1d0b5b8a30e744044304d57afb
DylanSandi <dsandi70404@ufide.ac.cr>
2024-03-04 17:13:56 -0600

    Validación de código (Primera parte del proyecto terminada)

--- Proyecto Mensajeria Fidelitas.py
+++ Proyecto Mensajeria Fidelitas.py
@@ -1,79 +1,180 @@
+
+opcion = 0
+opcionValida = False
+usuarioValido = False
+facturaValida = False
+salirValido = False
+paqueteValido = False
+
+
+print("\nBienvenido al sistema de mensajería fidélitas.")
+
 #Menú Principal
 
-opcion = 0
-
-print("Bienvenido al sistema de mensajería fidélitas.")
-print("\ningrese los credenciales de su usuario")
-
-#Aquí va el código de usuario
-
 while True:
+
+    opcionValida = False
+    opcion = 0
 
     print("\n------Menú------")
 
     print("\n1.Registrar un usuario")
     print("\n2.Registrar factura electrónica")
-    print("\n3.Salir del sistema")
-   
-    opcion = int(input("\nIngrese la opción deseada:"))
+    print("\n3.Crear paquete")
+    print("\n4.Salir del sistema")
 
+    #Validar opción
+    while opcionValida == False:
+
+        try:
+
+            opcion = int(input("\nIngrese la opción deseada:"))
+
+            if opcion > 4:
+
+                print("\nDato incorrecto, por favor ingrese una opción válida")
+                opcionValida = False 
+
+            else:
+
+                opcionValida = True
+
+        except:
+
+            print("\nDato incorrecto, por favor ingrese una opción válida")
+            opcionValida = False
+
+    #Formulario para usuario           
     if opcion == 1:
 
+        usuarioValido = False
+
+        #validar usuario
+        while usuarioValido == False:
+
+            try:
+
+                print("\nPor favor registre su cuenta de usuario")
+
+                print("")
+
+                correo=str(input("\nIngrese su correo electrónico:"))
+                comercio=str(input("\nIngrese el nombre del comercio:"))
+                telefono=int(input("\nIngrese el número de teléfono del comercio:"))
+                nombre=str(input("\nIngrese el nombre del usuario:"))
+                ubicacion=str(input("\nIngrese la dirección de comercio:"))
+
+                print("\n------Usuario registrado------")
+
+                print("\nSu correo es:",correo)
+                print("\nEl nombre del comercio es:",comercio)
+                print("\nEl teléfono del comercio es:",telefono)
+                print("\nEl nombre de usuario es:",nombre)
+                print("\nLa dirección del comercio es:",ubicacion)
+
+                usuarioValido = True
+
+            except:
+
+                print("\nDato incorrecto, por favor ingrese un dato válido")
+                usuarioValido = False
          
-         
-         print("Por favor registre su cuenta de usuario")
-         print("")
-         correo=str(input("Ingrese su correo electrónico:"))
-         comercio=str(input("Ingrese el nombre del comercio:"))
-         telefono=int(input("Ingrese el número de teléfono del comercio:"))
-         nombre=str(input("Ingrese el nombre del usuario:"))
-         ubicacion=str(input("Ingrese la dirección de comercio:"))
-         print("")
-         print("Su correo es:",correo)
-         print("El nombre del comercio es:",comercio)
-         print("El teléfono del comercio es:",telefono)
-         print("El nombre de usuario es:",nombre)
-         print("La dirección del comercio es:",ubicacion)
-
-
-        #Codigo de registro de factura electrónica
-
+    #Formulario para factura electrónica
     elif opcion == 2:
 
-        print("\nIngrese los siguientes datos:")
-        
-        print("Por favor complete los siguientes datos")
-        tipoced= str(input("Especifíque si Física o Jurídica: "))
-        cedula=int(input("Ingrese el número de cédula: " ))
-        empresa=str(input("Ingrese el nombre registrado: "))
-        telefo=int(input("Ingrese el número de teléfono: "))
-        correoemp=str(input("Ingrese el correo electrónico: "))
-        provincia=str(input("Ingrese la Provincia: "))
-        canton=str(input("Ingrese el Cantón: "))
-        distrito=str(input("Ingrese el Distrito: "))
-        print("")
-        print("Su tipo de cédula es: ",tipoced)
-        print("Su número de cédula es:",cedula)
-        print("El nombre registrado de comercio es: ",empresa)
-        print("El teléfono registrado es: ",telefo)
-        print("El correo empresarial es: ",empresa)
-        print("La Provincia es: ",provincia)
-        print("El Cantón  es: ",canton)
-        print("El Distrito es: ",distrito)
+        facturaValida = False
 
+        #validar Factura
+        while facturaValida == False:
+
+            try:
+
+                print("\nPor favor complete los siguientes datos")
+                
+                tipoced = input("\nEspecifíque si su cédula es Física o Jurídica: ")
+                cedula = input("\nIngrese el número de cédula: " )
+                empresa = input("\nIngrese el nombre registrado: ")
+                telefo = int(input("\nIngrese el número de teléfono: "))
+                correoemp = input("\nIngrese el correo electrónico: ")
+                provincia = input("\nIngrese la Provincia: ")
+                canton = input("\nIngrese el Cantón: ")
+                distrito = input("\nIngrese el Distrito: ")
+
+                print("\n------Factura creada con éxito------")
+
+                print("\nTipo de cédula:",tipoced)
+                print("\nNúmero de cédula:",cedula)
+                print("\nNombre registrado de comercio es:",empresa)
+                print("\nTeléfono registrado:",telefo)
+                print("\nCorreo empresarial es:",empresa)
+                print("\nProvincia:",provincia)
+                print("\nCantón:",canton)
+                print("\nDistrito: ",distrito)
+
+                facturaValida = True
+
+            except:
+
+                print("\nDato incorrecto, por favor ingrese un dato válido")
+                facturaValida = False
+
+    #Formulario para paquete           
     elif opcion == 3:
+
+        paqueteValido = False
+
+        #validar paquete 
+        while paqueteValido == False:
+
+            try:
+
+                print("\nIngrese los siguientes datos para crear el paquete:")
+                destinatario = input("\nNombre del destinatario: ")
+                telefono_destinatario = int(input("\nTeléfono del destinatario: "))
+                cedula_destinatario = input("\nNúmero de cédula del destinatario: ")
+                peso_paquete = float(input("\nPeso del paquete (Kilogramos): "))
+                cobro_contra_entrega = input("\nMonto en colones a cobrar contra entrega: ")
+
+                print("\n------Paquete creado con éxito------")
+                print("\nNombre del destinatario:", destinatario)
+                print("\nTeléfono del destinatario:", telefono_destinatario)
+                print("\nNúmero de cédula del destinatario:", cedula_destinatario)
+                print("\nPeso del paquete (Kilogramos):", peso_paquete)
+                print("\nMonto a cobrar contra entrega:", cobro_contra_entrega)
+
+                paqueteValido = True
+
+            except:
+
+                print("\nDato incorrecto, por favor ingrese un dato válido")
+
+                paqueteValido = False
+
+    elif opcion == 4:
 
         opcionSalir = 0
 
         while True:
+
+            salirValido = False
 
             print("\n¿Está seguro(a) que desea salir del sistema?")
 
             print("\n1.Si")
             print("\n2.No")
 
-            opcionSalir = int(input("\nIngrese la opción deseada:"))
-            
+            while salirValido == False:
+
+                try:
+
+                    opcionSalir = int(input("\nIngrese la opción deseada:"))
+                    salirValido = True
+
+                except:
+
+                    print("\nDato incorrecto, por favor ingrese un dato válido")
+                    salirValido = False
+
             if opcionSalir == 1:
 
                 print("\nGracias por utilizar nuestro sistema")
@@ -93,72 +194,3 @@ while True:
             break
         else:
             print("\nRegresando al menú.....")
-
-    else:
-
-        print("\nHa digitado una opción inválida, por favor intente de nuevo")
-# Función para la creación de paquetes
-def crear_paquete():
-    print("\nIngrese los siguientes datos para crear el paquete:")
-    destinatario = input("Nombre del destinatario: ")
-    telefono_destinatario = input("Teléfono del destinatario: ")
-    cedula_destinatario = input("Número de cédula del destinatario: ")
-    peso_paquete = float(input("Peso del paquete (Kilogramos): "))
-    cobro_contra_entrega = input("Monto en colones a cobrar contra entrega: ")
-    print("\nPaquete creado con éxito:")
-    print("Nombre del destinatario:", destinatario)
-    print("Teléfono del destinatario:", telefono_destinatario)
-    print("Número de cédula del destinatario:", cedula_destinatario)
-    print("Peso del paquete (Kilogramos):", peso_paquete)
-    print("Monto a cobrar contra entrega:", cobro_contra_entrega)
-
-opcion = 0
-
-print("Bienvenido al sistema de mensajería fidélitas.")
-print("\ningrese los credenciales de su usuario")
-
-# Aquí va el código de usuario
-
-while True:
-    print("\n------Menú------")
-    print("\n1.Registrar un usuario")
-    print("\n2.Registrar factura electrónica")
-    print("\n3.Salir del sistema")
-
-    opcion = int(input("\nIngrese la opción deseada:"))
-
-    if opcion == 1:
-        # Código para registrar un usuario
-        print("Por favor registre su cuenta de usuario")
-        # Aquí iría el código para registrar un usuario
-
-    elif opcion == 2:
-        # Código para registrar una factura electrónica
-        print("\nIngrese los siguientes datos:")
-        # Aquí iría el código para registrar una factura electrónica
-
-    elif opcion == 3:
-        opcionSalir = 0
-        while True:
-            print("\n¿Está seguro(a) que desea salir del sistema?")
-            print("\n1.Si")
-            print("\n2.No")
-            opcionSalir = int(input("\nIngrese la opción deseada:"))
-            if opcionSalir == 1:
-                print("\nGracias por utilizar nuestro sistema")
-                break
-            elif opcionSalir == 2:
-                break
-            else:
-                print("\nHa digitado una opción inválida, por favor intente de nuevo")
-        if opcionSalir == 1:
-            break
-        else:
-            print("\nRegresando al menú.....")
-
-    else:
-        print("\nHa digitado una opción inválida, por favor intente de nuevo")
-
-    # Llamar a la función para crear paquete si la opción es válida (opción 4)
-    if opcion == 4:
-        crear_paquete()
