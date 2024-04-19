94c509e6341f4d199af056e175974f18b3e88b52
DylanSandi <dsandi70404@ufide.ac.cr>
2024-04-19 16:41:10 -0600

    Entrega final

--- Proyecto Mensajeria Fidelitas.py
+++ Proyecto Mensajeria Fidelitas.py
@@ -195,7 +195,7 @@ def crearPaquete(matrizF, matrizP, guia, matrizG,arreglo):
 
             opcionFactura = int(input("\nIngrese el número del cleinte del que quiere crear el paquete:"))
 
-            if opcionFactura != num or opcionFactura > num or opcionFactura < 1:
+            if opcionFactura > num or opcionFactura < 1:
 
                 print("\n------Error------")
                 print("\npor favor ingrese una opcion válida")
@@ -367,7 +367,7 @@ def cambiarEstado(matriz):
 
             opcionCambiarP = int(input("\nIngrese el número del paquete al que desea cambiar su estado:"))
 
-            if opcionCambiarP != numC or opcionCambiarP > numC or opcionCambiarP < 1:
+            if opcionCambiarP > numC or opcionCambiarP < 1:
 
                 print("\n------Error!------")
                 print("\npor favor ingrese una opcion válida")
@@ -461,12 +461,328 @@ def restrearPaquete(matriz):
 
                 print("\nEstado actual del paquete:",matriz[fila][7], end = " ")
 
+#Función para mostrar la cantidad de envíos totales
+def cantEnvios(mPaquete):
+
+    cantEnvios = len(mPaquete)
+
+    print("\nCantidad de envíos registrados:",cantEnvios)
+
+#Función para mostrar la lista de paquetes enviados
+def listaPaquetesEnviados(mPaquete):
+
+    print("\nLista de paquetes enviados:")
+    print()
+    numPaquete = 0
+
+    for fila in range(len(mPaquete)):
+
+            for columna in range(len(mPaquete[fila])):
+
+                if mPaquete [fila][columna] == "Entregado":
+
+                    numPaquete += 1
+                    print("#",numPaquete)
+
+                    print(mPaquete[fila][3], end = " ")
+                    print(mPaquete[fila][1], end = " ")
+
+            print()
+
+#Función para mostrar el monto de cobro total
+def montoCobro(mPaquete):  
+
+    cobroTotal = 0
+
+    for fila in range(len(mPaquete)):
+
+        cobroTotal += mPaquete[fila][6]
+
+    print("Monto total a cobrar:₡",cobroTotal)
+
+#Función para mostrar la cantidad de paquetes por número de teléfono
+def cantPaquetesTel(mPaquete):
+
+    telSValido = False
+    numTelS = 0
+    coincide = 0
+
+    while telSValido == False:
+
+        try:
+            numTelS = int(input("\nIngrese el número de teléfono que desea buscar:"))
+
+            for fila in range(len(mPaquete)):
+
+                if numTelS == mPaquete[fila][2]:
+
+                    coincide+=1
+            
+            if coincide >=1:
+
+                print("\nEl número",numTelS,"Tiene un total de",coincide,"Paquetes registrados")
+                telSValido = True
+
+            else:
+
+                print("\nNo se encontró ningún número de teléfono registrado, por favor intente nuevamente")
+                telSValido == False
+            
+        except:
+
+            print("\n!!Error\n Por favor ingrese un número de teléfono válido")
+            telSValido =False
+
+#Función para mostrar la cantidad de paquetes por número de cédula
+def cantPaquetesCed(mPaquete):
+
+    cedSValido = False
+    cedS = 0
+    coincideCed = 0
+
+    while cedSValido == False:
+
+        try:
+
+            cedS = int(input("\nIngrese el número de cédula que desea buscar:"))
+
+            for fila in range(len(mPaquete)):
+
+                if cedS == mPaquete[fila][3]:
+
+                    coincideCed+=1
+            
+            if coincideCed >=1:
+
+                print("\nEl número de cédula",cedS,"Tiene un total de",coincideCed,"Paquetes registrados")
+                cedSValido = True
+
+            else:
+
+                print("\nNo se encontró ningún número de cédula registrado, por favor intente nuevamente")
+                cedSValido== False
+            
+        except:
+
+            print("\n!!Error\n Por favor ingrese un número de cédula válido")
+            cedSValido =False
+
+#Función con menú del módulo de estadística
+def moduloEstadistica(matrizP):
+
+    print("\n------Módulo de estadísitica------")
+    opcionMod = 0
+
+    while opcionMod !=6:
+
+        opcionMod = 0
+        opcionModValida = False
+
+        print("\n1.Cantidad de envíos")
+        print("\n2.Lista de paquetes enviados")
+        print("\n3.Monto total de cobro")
+        print("\n4.Buscar cantidad de paquetes por número de teléfono")
+        print("\n5.Buscar cantidad de paquetes por número de cédula")
+        print("\n6.Salir del módulo de estadística")
+
+        while opcionModValida == False:
+
+            try:
+                opcionMod = int(input("\nIngrese el número de estadística que desea observar:"))
+
+                if opcionMod >6:
+
+                    opcionModValida = False
+                    print("\nError!!\nIngrese un número de estadística válido")
+
+                else:
+
+                    opcionModValida = True 
+
+            except:
+
+                opcionModValida = False
+                print("\nError!!\nIngrese un número de estadística válido")
+        
+        if opcionMod == 1:
+
+            cantEnvios(matrizP)
+
+        elif opcionMod == 2:
+
+            listaPaquetesEnviados(matrizP)
+        
+        elif opcionMod == 3:
+
+            montoCobro(matrizP)
+
+        elif opcionMod == 4:
+
+            cantPaquetesTel(matrizP)
+
+        elif opcionMod == 5:
+
+            cantPaquetesCed(matrizP)
+        
+        else:
+
+            print("\nSaliendo.....\nHa salido satisfactoriamente del módulo de estadística")
+
+#Guardar datos de usuario en archivo
+def archivoUsuario(arregloU):
+
+    file = open("Usuario.txt", "a")
+
+    for pos in range(len(arregloU)):
+
+        if pos == 0:
+
+            file.write("\nCorreo electrónico:")
+            file.write(arregloU[pos])
+
+        elif pos == 1:
+
+            file.write("\nNombre del comercio:")
+            file.write(arregloU[pos])
+
+        elif pos == 2:
+
+            file.write("\nTeléfono de comercio:")
+            file.write(str(arregloU[pos]))
+
+        elif pos == 3:
+
+            file.write("\nNombre de dueño:")
+            file.write(arregloU[pos])
+
+        elif pos == 4:
+
+            file.write("\nUbicación de local:")
+            file.write(arregloU[pos])
+
+    file.write("\n\n")
+    file.close()
+
+#Guardar datos de factura en archivo
+def archivoFactura(matrizFactura,filas):
+
+    file = open("Factura.txt", "a")
+
+    for fila in range(len(matrizFactura)):
+
+        for columna in range(len(matrizFactura[fila])):
+            
+            if fila == filas:
+                
+                if columna == 0:
+
+                    file.write("\nTipo de cédula:")
+                    file.write(matrizFactura[fila][columna])
+
+                elif columna == 1:
+
+                    file.write("\nNúmero de cédula:")
+                    file.write(str(matrizFactura[fila][columna]))
+
+                elif columna == 2:
+
+                    file.write("\nNombre registrado:")
+                    file.write(matrizFactura[fila][columna])
+
+                elif columna == 3:
+
+                    file.write("\nTeléfono:")
+                    file.write(str(matrizFactura[fila][columna]))
+
+                elif columna == 4:
+
+                    file.write("\nCorreo:")
+                    file.write(matrizFactura[fila][columna])
+
+                elif columna == 5:
+
+                    file.write("\nProvincia:")
+                    file.write(matrizFactura[fila][columna])
+
+                elif columna == 6:
+
+                    file.write("\nCantón:")
+                    file.write(matrizFactura[fila][columna])
+
+                elif columna == 7:
+
+                    file.write("\nDistrito:")
+                    file.write(matrizFactura[fila][columna])
+
+    file.write("\n\n")
+    file.close()
+
+#Guardar datos de paquete en archivo
+def archivoPaquete(matrizPaquete,filas):
+
+    file = open("Paquete.txt", "a")
+
+    for fila in range(len(matrizPaquete)):
+
+        for columna in range(len(matrizPaquete[fila])):
+            
+            if fila == filas:
+                
+                if columna == 0:
+
+                    file.write("\nNúmero de guía:")
+                    file.write(str(matrizPaquete[fila][columna]))
+
+                elif columna == 1:
+
+                    file.write("\nNombre de destinatario:")
+                    file.write(matrizPaquete[fila][columna])
+
+                elif columna == 2:
+
+                    file.write("\nTeléfono de destinatario:")
+                    file.write(str(matrizPaquete[fila][columna]))
+
+                elif columna == 3:
+
+                    file.write("\nNúmero de cédula de destinatario:")
+                    file.write(str(matrizPaquete[fila][columna]))
+
+                elif columna == 4:
+
+                    file.write("\nPeso del paquete:")
+                    file.write(str(matrizPaquete[fila][columna]))
+
+                elif columna == 5:
+
+                    file.write("\nPago:")
+                    file.write(matrizPaquete[fila][columna])
+
+                elif columna == 6:
+
+                    file.write("\nMonto a pagar:")
+                    file.write(str(matrizPaquete[fila][columna]))
+
+                elif columna == 7:
+
+                    file.write("\nEstado de paquete:")
+                    file.write(matrizPaquete[fila][columna])
+
+    file.write("\n\n")
+    file.close()
+
+
 #Principal
 print("\n------Bienvenido al sistema especializado de control de mensajerías------")
 
-registrarUsuario(usuario)
+filaFactura = 0
+filaPaquete = 0
 
-while opcion != 5 :
+
+registrarUsuario(usuario)
+archivoUsuario(usuario)
+
+while opcion != 6 :
 
     opcionValida = False
 
@@ -476,7 +792,8 @@ while opcion != 5 :
     print("\n2.Crear paquete")
     print("\n3.Cambiar estado de paquete")
     print("\n4.Rastrear un paquete")
-    print("\n5.Salir del sistema")
+    print("\n5.Módulo de estadística")
+    print("\n6.Salir del sistema")
 
     while opcionValida == False:
 
@@ -484,7 +801,7 @@ while opcion != 5 :
 
             opcion = int(input("\nIngrese la opción que desea realizar:"))
 
-            if opcion > 5:
+            if opcion > 6:
 
                 print("\n------Error------")
                 print("\n Por favor ingrese una opción válida")
@@ -501,8 +818,18 @@ while opcion != 5 :
 
     if opcion == 1:
 
-        registrarFactura (factura)
-    
+        if not factura:
+
+            filaFactura = 0
+            registrarFactura(factura)
+            archivoFactura(factura,filaFactura)
+
+        else:
+
+            filaFactura += 1
+            registrarFactura(factura)
+            archivoFactura(factura,filaFactura)
+
     elif opcion == 2:
 
         if not factura:
@@ -511,11 +838,20 @@ while opcion != 5 :
             print("\nAntes de crear un paquete, debe primero registar los datos para una factura electrónica")
 
         else:
+            
+            if not paquete:
 
-            numGuia +=1
+                numGuia +=1
+                filaPaquete = 0
+                crearPaquete(factura,paquete,numGuia,guias,usuario)
+                archivoPaquete(paquete,filaPaquete)
 
-            crearPaquete(factura,paquete,numGuia,guias,usuario)
-            mostrarPaquete(paquete)
+            else:
+
+                numGuia +=1
+                filaPaquete += 1
+                crearPaquete(factura,paquete,numGuia,guias,usuario)
+                archivoPaquete(paquete,filaPaquete)
 
     elif opcion ==3:
 
@@ -538,6 +874,18 @@ while opcion != 5 :
         else:
 
             restrearPaquete(paquete)
+
+    elif opcion == 5:
+
+        if not paquete:
+
+            print("\n------ERROR------")
+            print("\nNo ha creado ningún paquete")
+
+        else:
+
+            moduloEstadistica(paquete)
+
     else:
 
         print("\nGracias por utilizar el sistema")
